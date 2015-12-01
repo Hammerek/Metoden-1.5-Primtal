b78ffd717d59999ffce7cff91aab65160e0cc49b
Hammerek <anakin165@wp.pl>
2015-12-01 18:56:28 +0100

    Primtal

---
+++ Metoden 1.5 Primtal/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metoden_1._5_Primtal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tbx1 = Convert.ToInt32(textBox1.Text);
+            primtal(); 
+        }
+
+        private int primtal()
+        {
+            int tal = 0;
+            tal = Convert.ToInt32(textBox1.Text);
+            if (tal > 0 && tal < 3)
+            {
+              return  MessageBox.Show("Talet är ett primtal");
+            }
+            if (tal % 2 == 0)
+            {
+                MessageBox.Show("Talet är inte ett primtal");
+            }
+            for (int i = 3; i < tal / 2; i++)
+            {
+                if (tal % 1 == 0)
+                {
+                    MessageBox.Show("Talet är inte ett primtal");
+                }
+                MessageBox.Show("Talet är ett primtal");
+            }
+        }
+
+    }
+}
